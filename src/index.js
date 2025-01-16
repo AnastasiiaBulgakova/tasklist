7441f9626007bed0b8c754bf753e141c32f3d625
Anastasiia Bulgakova <nbulghakova@mail.ru>
2025-01-17 00:11:04 +0300

    laast changdfe

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-// import ReactDOM from "react-dom";
 import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx'
 
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-
-createRoot(document.getElementById('root')).render(
-
-    <App />,
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
