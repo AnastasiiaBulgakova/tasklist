251c7eebf83666a2a17f681d83e857afbda11d17
Anastasiia Bulgakova <nbulghakova@mail.ru>
2025-01-17 00:29:04 +0300

    fdkls

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.jsx'
+// import ReactDOM from "react-dom";
+import { React, StrictMode } from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
